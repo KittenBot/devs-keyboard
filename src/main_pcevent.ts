4b577bc57afc825e80ef91e42562c974c34379c1
Riven <4771215@qq.com>
2023-11-09 14:11:17 +0800

    pc events works~

--- src/main_pcevent.ts
+++ src/main_pcevent.ts
@@ -6,7 +6,9 @@ const cd = await new ds.PCEvent()
 const button = new ds.Button()
 
 button.down.subscribe(async () => {
-    await cd.openUrl("https://www.google.com")
+    // await cd.openUrl("https://www.google.com")
+    // await cd.startApp("calc.exe")
+    await cd.sendText("[捂脸]\n")
 })
 
 
