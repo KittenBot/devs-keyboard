a4bf55aded5676d73fb045218f931566557957e1
riven <4771215@qq.com>
2023-12-27 11:55:56 +0800

    update sdl renderer

--- src/utils/sdl_display.ts
+++ src/utils/sdl_display.ts
@@ -10,18 +10,20 @@ export class SDLDisplay implements Display {
     palette: Palette
     constructor(public image: Image) {
         assert(image.bpp === 4, "SDLDisplay only supports 4bpp images")
-
+        this.image = image
+        this.palette = Palette.arcade()
     }
     
     async init() {
         console.log(`SDLDisplay.init()`)
+        console.log("image", this.image.width, this.image.height)
     }
 
     async show() {
         await (ds as DsSpi).spiSendImage(
             this.image,
             this.palette.buffer,
-            0
+            0x1
         )
     }
 }
